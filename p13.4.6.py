68b749043492a980c0d778069066210f521e5d8e
Racoon4ik <artem.kurgan20220@gmail.com>
2026-07-16 15:39:12 +0300

    Задача 13.2.2 добавлена

---
+++ p13.4.6.py
@@ -0,0 +1,17 @@
+# объявление функции
+def get_factors(num):
+    factors = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            factors.append(i)
+    return factors
+
+def number_of_factors(num):
+# вызываем ранее созданную функцию и возвращаем длину её списка
+    return len(get_factors(num))
+
+# считываем данные
+n = int(input())
+
+# вызываем функцию
+print(number_of_factors(n))
