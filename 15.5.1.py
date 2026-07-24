9a01dfa5dcccc2cff03c0d2a2d022faffe7d085a
Racoon4ik <artem.kurgan20220@gmail.com>
2026-07-24 14:57:35 +0300

    Обновлена задача

--- 15.5.1.py
+++ 15.5.1.py
@@ -1,28 +1,32 @@
-eng = "abcdefghijklmnopqrstuvwxyz"
-rus = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
-symbols = [" ", ",", ".", "!", "?"]
-lang = input("Выберите язык (а - англ.; р - русс.) ")
-chs = input("Шифрование (ш) или дешифрование (д)? ")
+lang = input("Выберите язык (а - англ.; р - русс.): ").lower()
+choose = input("Шифрование (ш) или дешифрование (д)? ").lower()
 k = int(input("Какой сдвиг? "))
 text = input("Введите текст: ")
-def process_shifra(n, langg, shifr, phrase):
-    phrase = phrase.lower()
-    if langg == "р":
-        alphabet = rus
-        mochnost = 32
-    else:
-        alphabet = eng
-        mochnost = 26
-    if shifr == 'д':
+def process_shifra(n, lang, answer, phrase):
+    if answer == 'д':
         n = -n
     result = ""
     for char in phrase:
-        if char in alphabet:
-            x = alphabet.index(char)
-            y = (x + n) % mochnost
-            result += alphabet[y]
+        if lang == "а":
+            if 'A' <= char <= 'Z':
+                start = ord('A')
+                result += chr((ord(char) - start + n) % 26 + start)
+            elif 'a' <= char <= 'z':
+                start = ord('a')
+                result += chr((ord(char) - start + n) % 26 + start)
+            else:
+                result += char
+        elif lang == "р":
+            if 'А' <= char <= 'Я':
+                start = ord('А')
+                result += chr((ord(char) - start + n) % 32 + start)
+            elif 'а' <= char <= 'я':
+                start = ord('а')
+                result += chr((ord(char) - start + n) % 32 + start)
+            else:
+                result += char
         else:
-            result += char
+            result += char 
     return result
-final_text = process_shifra(k, lang, chs, text)
+final_text = process_shifra(k, lang, choose, text)
 print("Результат:", final_text)
